24b8998426e93dfcc3023b3b5e197eab747057b9
aeldridg42 <vv.azat@yandex.ru>
2023-02-20 19:29:47 +0300

    SQL script execution added

--- ex02/Cinema/src/main/java/edu/school21/cinema/config/ApplicationConfig.java
+++ ex02/Cinema/src/main/java/edu/school21/cinema/config/ApplicationConfig.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Objects;
 
 @Configuration
@@ -25,21 +29,25 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource() throws SQLException {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
         dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("db.driverClassName")));
         dataSource.setUrl(env.getProperty("db.url"));
         dataSource.setUsername(env.getProperty("db.username"));
         dataSource.setPassword(env.getProperty("db.password"));
+        Connection connection = dataSource.getConnection();
+        ScriptUtils.executeSqlScript(connection, new ClassPathResource("sql/schema.sql"));
 
         return dataSource;
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate() {
+    public JdbcTemplate jdbcTemplate() throws SQLException {
         return new JdbcTemplate(dataSource());
     }
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
